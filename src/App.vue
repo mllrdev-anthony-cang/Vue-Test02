0f7fb9d867f3189ef99f0d31de9d98932205894c
mllrdev-anthony-cang <62974176+mllrdev-anthony-cang@users.noreply.github.com>
2023-05-09 13:41:57 +0800

    apply kebab case

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/Hello-World.vue'
 </script>
 
 <template>
